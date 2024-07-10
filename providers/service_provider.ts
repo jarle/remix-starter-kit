865e3f555a0ea1f02f43dd099bbc3e460455f446
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-07-10 18:44:32 +0200

    support custom service resolvers

--- providers/service_provider.ts
+++ providers/service_provider.ts
@@ -5,19 +5,33 @@ export default class ServiceProvider {
   constructor(protected app: ApplicationService) {}
 
   register() {
-    Object.entries(ServiceProviders).forEach(([key, creator]) => {
+    Object.entries(ServiceProviders).forEach(([key, lazyServiceProvider]) => {
       this.app.container.singleton(key as any, async (resolver) => {
-        const constructor = await creator()
-        return resolver.make(constructor.default)
+        const provider = await lazyServiceProvider()
+        if (this.isConstructorProvider(provider)) {
+          return resolver.make(provider.default)
+        }
+
+        return resolver.make(provider)
       })
     })
   }
+
+  private isConstructorProvider(module: any) {
+    return module && typeof module === 'object' && 'default' in module && typeof module.default === 'function'
+  }
 }
 
+type UnwrapPromise<T> = T extends Promise<infer U> ? U : T;
+
+type UnwrapModule<T> = T extends { default: infer U } ? U : T;
+
 type ProvidedServices = {
-  [K in keyof typeof ServiceProviders]: InstanceType<
-    Awaited<ReturnType<(typeof ServiceProviders)[K]>>['default']
-  >
+  [K in keyof typeof ServiceProviders]: UnwrapModule<
+    UnwrapPromise<ReturnType<(typeof ServiceProviders)[K]>>
+  > extends new (...args: any[]) => infer R
+  ? R
+  : UnwrapModule<UnwrapPromise<ReturnType<(typeof ServiceProviders)[K]>>>
 }
 
 declare module '@adonisjs/core/types' {
