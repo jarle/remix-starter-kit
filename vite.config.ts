6ba7b846f974570fb2f7cc5d5bfd63ee9766db3c
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-08-16 17:42:26 +0200

    remove vite base config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig(({ isSsrBuild }) => ({
-  base: '/assets/',
   plugins: [
     remix({
       appDirectory: 'resources/remix_app',
