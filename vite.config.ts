b4bc1645227e1e9616b3d5f05c239a548f9ee2fe
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2024-12-15 12:34:32 +0100

    use rr7 adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,10 @@
-import { vitePlugin as remix } from '@remix-run/dev';
+import { reactRouter } from '@react-router/dev/vite';
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ isSsrBuild }) => ({
   plugins: [
-    remix({
-      appDirectory: 'resources/remix_app',
-      buildDirectory: 'build/remix',
-      serverBuildFile: 'server.js',
-      future: {
-        v3_singleFetch: true,
-        v3_fetcherPersist: true,
-        v3_relativeSplatPath: true,
-        v3_throwAbortReason: true,
-        v3_lazyRouteDiscovery: true,
-      }
-    }),
+    reactRouter(),
     tsconfigPaths(),
   ],
   optimizeDeps: {
@@ -26,10 +15,3 @@ export default defineConfig(({ isSsrBuild }) => ({
       : {},
   },
 }))
-
-declare module "@remix-run/node" {
-  // https://remix.run/docs/en/main/guides/single-fetch#enable-single-fetch-types
-  interface Future {
-    v3_singleFetch: true;
-  }
-}
