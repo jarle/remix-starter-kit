cc4c7493ee8242a59aeaba87c34b4e5c9d1630b5
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-01-15 08:20:31 +0100

    update dependencies and config

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { reactRouter } from '@react-router/dev/vite';
+import autoprefixer from "autoprefixer";
+import tailwindcss from "tailwindcss";
 import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig(({ isSsrBuild }) => ({
+  css: {
+    postcss: {
+      plugins: [tailwindcss, autoprefixer],
+    },
+  },
   plugins: [
     reactRouter(),
     tsconfigPaths(),
