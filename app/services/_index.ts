8d61ae4288cf566e494bfaef8c549c714d04d218
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-03-20 07:49:17 +0100

    commit

--- app/services/_index.ts
+++ app/services/_index.ts
@@ -3,5 +3,5 @@ import type { LazyService } from '#providers/service_provider';
 // Register services that should be available in the container here
 export const ServiceProviders = {
   env: () => import('#start/env'),
-  hello_service: () => import('./hello_service.js'),
+  hello_service: () => import('#services/hello_service'),
 } satisfies Record<string, LazyService>
