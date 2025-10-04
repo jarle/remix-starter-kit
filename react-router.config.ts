0410960675f77451418881b6c04a37468ef6dcb5
Jarle Mathiesen <jarle.mathiesen@gmail.com>
2025-10-04 21:21:34 +0200

    commit

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,6 @@ import type { Config } from '@react-router/dev/config'
 export default {
   ssr: true,
   appDirectory: 'resources/react_app',
-  buildDirectory: 'build/react_router',
+  buildDirectory: 'build/react-router',
   serverBuildFile: 'server.js',
 } satisfies Config
